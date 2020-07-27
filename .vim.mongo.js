499bdd853e68e55f91eeea35e5a3644b32a12d91
nimbus117 <mail@nimbus117.co.uk>
2020-07-27 23:07:42 +0100

    adds example .vim.mongo.js

---
+++ .vim.mongo.js
@@ -0,0 +1,42 @@
+/**
+ * Example usage
+ *
+ * Set the database connection string
+ * :let b:db = "mongodb://user:password@host/database"
+ *
+ * Use :DB or <c-j> to execute 1 or more lines
+ * For multi line commands use a range or visual mode
+ */
+
+// <c-j> or :DB
+help
+
+// vip<c-j> or :11,15DB
+db.example.insertOne({
+  name: "Person 1",
+  age: 1,
+  email: "person1@example.com",
+})
+
+db.example.find()
+
+const arr = [];
+for (let x = 2; x <= 100; x++) {
+  arr.push({
+    name: `Person ${x}`,
+    age: x,
+    email: `person${x}@example.com`,
+  });
+}
+db.example.insertMany(arr)
+
+db.example.find(
+  {
+    age: { $lte: 35 }
+  },
+  {
+    name: 1, age: 1, _id: 0 
+  }
+).map((t) => [t.name, t.age])
+
+db.example.drop()
